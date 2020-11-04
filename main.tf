63f66ef14788359c5adac1c1cbb5521c3b67778a
Costas Tyfoxylos <costas.tyf@gmail.com>
2020-11-04 12:01:53 +0100

    added a small readme, fix list length checking and removed use of explicit tag

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_ram_resource_share" "subnet_sharing" {
-  count                     = length(var.private_subnet_arns) >= 0 || length(var.public_subnet_arns) >= 0 ? 1 : 0
+  count                     = length(var.private_subnet_arns) > 0 || length(var.public_subnet_arns) > 0 ? 1 : 0
   name                      = "${var.prepend_resource_type ? "resource-share-" : ""}subnets-${var.name}"
   allow_external_principals = true
-  tags                      = merge({ resource-type = "ec2:Subnet" }, var.tags)
+  tags                      = var.tags
 }
 
 resource "aws_ram_resource_association" "private_subnets" {
