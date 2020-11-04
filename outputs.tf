0546e7d65e72180830b79eb2ea7c2202a7b533ec
Costas Tyfoxylos <costas.tyf@gmail.com>
2020-11-04 12:04:31 +0100

    made output safe

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
-  value       = aws_ram_resource_share.subnet_sharing[0].id
+  value       = try(aws_ram_resource_share.subnet_sharing[0].id, null)
   description = "The ID of the subnet share in resource access manager"
 }
 
 output "arn" {
-  value       = aws_ram_resource_share.subnet_sharing[0].arn
+  value       = try(aws_ram_resource_share.subnet_sharing[0].id, null)
   description = "The ARN of the subnet share in resource access manager"
 }
