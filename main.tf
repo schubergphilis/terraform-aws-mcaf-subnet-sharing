3be5e9498177f869426f4053bcab8a71d40135d5
ghawker <ghawker@schubergphilis.com>
2020-11-09 14:05:18 +0100

    put count back

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
 resource "aws_ram_resource_share" "subnet_sharing" {
-  count                     = length(var.private_subnet_arns) > 0 || length(var.public_subnet_arns) > 0 ? 1 : 0
+  count                     = length(var.private_subnet_arns) >= 0 || length(var.public_subnet_arns) >= 0 ? 1 : 0
   name                      = "${var.prepend_resource_type ? "resource-share-" : ""}subnets-${var.name}"
   allow_external_principals = true
-  tags                      = var.tags
+  tags                      = merge({ resource-type = "ec2:Subnet" }, var.tags)
 }
-
 resource "aws_ram_resource_association" "private_subnets" {
-  for_each           = toset(var.private_subnet_arns)
-  resource_arn       = each.value
+  count              = length(var.private_subnet_arns)
+  resource_arn       = var.private_subnet_arns[count.index]
   resource_share_arn = aws_ram_resource_share.subnet_sharing[0].arn
 }
-
 resource "aws_ram_resource_association" "public_subnets" {
-  for_each           = toset(var.public_subnet_arns)
-  resource_arn       = each.value
+  count              = length(var.public_subnet_arns)
+  resource_arn       = var.public_subnet_arns[count.index]
   resource_share_arn = aws_ram_resource_share.subnet_sharing[0].arn
 }
