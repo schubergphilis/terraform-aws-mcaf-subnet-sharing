a4b9f3d639d8193eb602777fba2effd2bab938e1
Costas <costas.tyf@gmail.com>
2021-02-17 13:14:16 +0100

    Update main.tf
    using for_each since no count is used any more.

--- main.tf
+++ main.tf
@@ -3,13 +3,15 @@ resource "aws_ram_resource_share" "subnet_sharing" {
   allow_external_principals = true
   tags                      = var.tags
 }
+
 resource "aws_ram_resource_association" "private_subnets" {
-  count              = length(var.private_subnet_arns)
-  resource_arn       = var.private_subnet_arns[count.index]
+  for_each           = toset(var.private_subnet_arns)
+  resource_arn       = each.value
   resource_share_arn = aws_ram_resource_share.subnet_sharing.arn
 }
+
 resource "aws_ram_resource_association" "public_subnets" {
-  count              = length(var.public_subnet_arns)
-  resource_arn       = var.public_subnet_arns[count.index]
+  for_each           = toset(var.public_subnet_arns)
+  resource_arn       = each.value
   resource_share_arn = aws_ram_resource_share.subnet_sharing.arn
 }
